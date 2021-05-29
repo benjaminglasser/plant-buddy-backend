6475db779bed27e0ca5533a4c0686fc1415eec64
ben glasser <ben@fantasticvoyage.com>
2021-05-28 22:58:50 -0700

    update feat

--- controllers/buddies.js
+++ controllers/buddies.js
@@ -2,7 +2,9 @@ var Buddy = require('../models/buddy')
 
 module.exports = {
     index,
-    create
+    create,
+    delete: deleteBuddy,
+    update
 }
 
 function index(req, res) {
@@ -14,5 +16,17 @@ function index(req, res) {
 function create(req, res) {
     Buddy.create(req.body, function (err, buddy) {
         res.status(201).json(buddy)
+    })
+}
+
+function deleteBuddy(req, res) {
+    Buddy.findByIdAndDelete(req.params.id, function () {
+        index(req, res);
+    })
+}
+
+function update(req, res) {
+    Buddy.findByIdAndUpdate(req.params.id, req.body, function () {
+        index(req, res);
     })
 }
--- routes/buddies.js
+++ routes/buddies.js
@@ -5,5 +5,7 @@ var buddiesCtrl = require('../controllers/buddies')
 /* GET users listing. */
 router.get('/', buddiesCtrl.index)
 router.post('/', buddiesCtrl.create)
+router.put('/:id', buddiesCtrl.update)
+router.delete('/:id', buddiesCtrl.delete)
 
 module.exports = router;
